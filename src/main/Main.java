c51b387aebda535098f9f02f939f892d37cc1273
Skyfallin <dimetrius.hightower@gmail.com>
2020-11-22 16:45:05 -0700

    pushed basic window

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,27 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("MenuLookDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Display the window.
+        frame.setSize(450, 260);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(Main::createAndShowGUI);
+    }
 }
