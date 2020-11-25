c2ec1fca4aef1e79755d4a6b0ccbef3a25af50c7
Skyfallin <dimetrius.hightower@gmail.com>
2020-11-24 17:37:48 -0700

    added about button functionality

--- src/main/RosterView.java
+++ src/main/RosterView.java
@@ -1,14 +1,19 @@
 package main;
 
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
 public class RosterView extends JFrame {
+
+    private final JDialog aboutDialogue;
 
     /**
      *
      */
     public RosterView() {
         super("CSE360 Final Project");
+        this.aboutDialogue = createAboutDialog();
         // Create and set up the window.
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // Create and set up the content pane.
@@ -29,6 +34,7 @@ public class RosterView extends JFrame {
      * @return
      */
     private JMenuBar createMenuBar() {
+
         JMenuBar menuBar;
         JMenu fileMenu, aboutMenu;
         JMenuItem rosterItem, attendanceItem, saveItem, dataItem;
@@ -47,8 +53,37 @@ public class RosterView extends JFrame {
         fileMenu.add(dataItem);
 
         aboutMenu = new JMenu("About");
+        aboutMenu.addMenuListener(aboutMenuListener());
         menuBar.add(aboutMenu);
 
         return menuBar;
+    }
+
+    private JDialog createAboutDialog() {
+        JDialog jDialog = new JDialog(this, "About");
+        JLabel jLabel = new JLabel("This is our final project!");
+        jDialog.add(jLabel);
+        jDialog.setSize(200, 200);
+        jDialog.setVisible(false);
+        return jDialog;
+    }
+
+    private MenuListener aboutMenuListener() {
+        return new MenuListener() {
+            @Override
+            public void menuSelected(MenuEvent e) {
+                getAboutDialogue().setVisible(true);
+            }
+
+            @Override
+            public void menuDeselected(MenuEvent e) {}
+
+            @Override
+            public void menuCanceled(MenuEvent e) {}
+        };
+    }
+
+    public JDialog getAboutDialogue() {
+        return aboutDialogue;
     }
 }
